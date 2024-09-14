3cb75f3b1255c31e2f7647ecb3c3278a2d4c7334
iamleeon <165953114+iamleeon@users.noreply.github.com>
2024-09-14 17:28:55 +0300

    moved connection credentials to .env

--- src/database/db.py
+++ src/database/db.py
@@ -2,8 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 import psycopg2
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:567234@localhost:5432/contacts_app"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
